298577bfc52f783fe6b029047961c92890a63b37
David Aradom <davidaradom@yahoo.com>
2019-10-11 22:20:34 -0500

    query update

--- query.sql
+++ query.sql
@@ -3,7 +3,28 @@ SELECT * FROM influence;
 SELECT * FROM draft;
 
 -- Join tables on player_name
-SELECT draft.player_name, draft.draftYear, draft.draftRound, draft.draftOverall, draft.draftFrom
-FROM draft
-INNER JOIN influence
-ON UPPER(draft.player_name) = UPPER(influence.player_name);
+SELECT influence.player_name, influence."AGE", influence."W_PCT", influence."SALARY_MILLIONS", influence."TWITTER_FOLLOWER_COUNT_MILLIONS"
+FROM influence
+INNER JOIN draft
+ON UPPER(influence.player_name) = UPPER(draft.player_name);
+
+-- Sort by highest twitter follower count
+SELECT influence.player_name, influence."AGE", influence."W_PCT", influence."SALARY_MILLIONS", influence."TWITTER_FOLLOWER_COUNT_MILLIONS"
+FROM influence
+INNER JOIN draft
+ON UPPER(influence.player_name) = UPPER(draft.player_name)
+ORDER BY influence."TWITTER_FOLLOWER_COUNT_MILLIONS" DESC;
+
+-- Sort by highest salary
+SELECT influence.player_name, influence."AGE", influence."W_PCT", influence."SALARY_MILLIONS", influence."TWITTER_FOLLOWER_COUNT_MILLIONS"
+FROM influence
+INNER JOIN draft
+ON UPPER(influence.player_name) = UPPER(draft.player_name)
+ORDER BY influence."SALARY_MILLIONS" DESC;
+
+-- Sort by highest winning percentage
+SELECT influence.player_name, influence."AGE", influence."W_PCT", influence."SALARY_MILLIONS", influence."TWITTER_FOLLOWER_COUNT_MILLIONS"
+FROM influence
+INNER JOIN draft
+ON UPPER(influence.player_name) = UPPER(draft.player_name)
+ORDER BY influence."W_PCT" DESC;
